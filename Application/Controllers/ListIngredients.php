a96fe10074eb7aee939dd00648a0f3b82b59f251
Dev2Choiz <dev2choiz@gmail.com>
2015-02-26 16:48:40 +0100

    majre7

--- Application/Controllers/ListIngredients.php
+++ Application/Controllers/ListIngredients.php
@@ -41,8 +41,16 @@ class ListIngredients extends \Library\Controller\Controller {
     }
 
     public function updatelistingredients($params) {
+        //il y a aussi params['id_recette']
+
+        //supression de tout les ingredients
+        $this->deletelistingredients($params['id_recette']);
+
 
         unset($params['method']);
+
+
+        //insertion des nouveaux ingredients
         $modelLI  = new \Application\Models\ListIngredients('localhost');
 
         //var_dump($params);
@@ -50,7 +58,7 @@ class ListIngredients extends \Library\Controller\Controller {
         $params['unites']=json_decode($params['unites']) ;
         $params['quantites']=json_decode($params['quantites']) ;
         
-        $res=$modelLI->updateListIngredients($params);
+        $res=$modelLI->insertListIngredients($params);
             
         if($res ) {
             return $this->setApiResult( $res );
@@ -61,5 +69,25 @@ class ListIngredients extends \Library\Controller\Controller {
 
     }
 
+    public function deletelistingredients($idRecette) {
+        //il y a aussi param['id_recette']
 
+        
+
+
+
+        
+        $modelLI  = new \Application\Models\ListIngredients('localhost');
+
+        
+        $res=$modelLI->delete(" `id_recette`='$idRecette'");
+            
+        if($res ) {
+            return $this->setApiResult( $res );
+        }else{
+            return $this->setApiResult(false, true, "erreur pendant la suppression des ingredients");
+        }
+
+
+    }
 }
