4e5584e87d93304583dc615be5459f6ebaace131
Dev2Choiz <dev2choiz@gmail.com>
2015-03-06 11:02:43 +0100

    gestion unite et ingredient

--- Application/Controllers/Ingredient.php
+++ Application/Controllers/Ingredient.php
@@ -16,8 +16,6 @@ class Ingredient extends \Library\Controller\Controller {
      */
     public function __construct() {
         parent::__construct();
-        echo "classe trouve";
-        var_dump($_POST);
     }
 
 
@@ -53,13 +51,13 @@ class Ingredient extends \Library\Controller\Controller {
      * @return [int]         [l'id de l'ingredient ajouté]
      */
     public function insertingredients($params) {
-        echo "methode insert";
+        
         unset($params['method']);
 
         $modelIngredient  = new \Application\Models\Ingredient('localhost');
         
         $res=$modelIngredient->insert($params);
-            
+            var_dump($res);
         if(  $res  ) {
             return $this->setApiResult($modelIngredient->getLast());
         }else{
@@ -71,6 +69,41 @@ class Ingredient extends \Library\Controller\Controller {
 
 
 
+    public function updateingredient($params) {
+        
+        unset($params['method']);
+
+        $modelIngredient  = new \Application\Models\Ingredient('localhost');
+
+        
+        $id=$params['id_ingredient'];
+
+        $res=$modelIngredient->update(" `id_ingredient`={$id} ", $params);
+            
+        if(  $res  ) {
+            return $this->setApiResult(true);
+        }else{
+            return $this->setApiResult(false, true, "erreur pendant l'insertion des ingredients");
+        }
+
+
+    }
+
+
+    public function deleteingredient($params) {
+        
+        unset($params['method']);
+
+        $modelIngredient  = new \Application\Models\Ingredient('localhost');
+        
+        $res=$modelIngredient->delete(" `id_ingredient`={$params['id_ingredient']} ");
+            
+        if(  $res  ) {
+            return $this->setApiResult(true);
+        }else{
+            return $this->setApiResult(false, true, "erreur pendant la suppression de l'ingredient");
+        }
+    }
 
 
 
